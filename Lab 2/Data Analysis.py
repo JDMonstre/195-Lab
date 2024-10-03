edbd9d7fdb43d07dedc67cf333ba0c9353f0a117
JDMonstre <joss.demonts@gmail.com>
2024-10-03 12:55:37 -0700

    Finished easy math
    Finished the youngs modulus/ultimate tensile calculations and initial plotting.  Need to shift over the graphs now

--- Lab 2/Data Analysis.py
+++ Lab 2/Data Analysis.py
@@ -23,10 +23,13 @@ files = sorted(files)
 for n in files:
     raw_data.append(pd.read_csv(str(n), skiprows = 6))
     #print(str(n))
+    del n
+
+del location, full, files
     
 
 
-#%% Cleaning the data
+#%% Cleaning the data and creating a dataframe
 # Sticking everying in a dictionary with Name Area, height
 # keeps things tidier instead of having variables all over the place
 
@@ -39,12 +42,7 @@ samples = {
     }
 
 
-
-#%% Converting raw_data to stress_strain dataframe
-
-# Key is the sample name
-# value is strain - stress tuple
-
+# initializing the dataframe with the following column names
 dataframe = pd.DataFrame(columns=['Name', 'Area', 'Length'
                                   , 'Stress (MPa)','Strain'])
 
@@ -60,19 +58,71 @@ for index, value in enumerate(samples):
     
     
     # combining all the data into a single row
-    dank = [name, area, length, stress, stress]
+    dank = [name, area, length, stress, strain]
     # adding the data to the dataframe
     dataframe.loc[len(dataframe)] = dank
+    
+    # clearing variables so they don't appear in my IDE
+    del name, dimensions, area, length, data, strain, stress, dank
+    del index, value
+
+# Dataframe is the pandas dataframe with all the data
+
+del raw_data, samples
+
+#%% Youngs modulus, ultimate tensile strength
+
+# adding youngs modulus, intercept, and ultimate tensile to the dataframe
+dataframe['Youngs'] = 'Unknown'
+dataframe['Intercepts'] = 'Unknown'
+dataframe['Ultimate'] = 'Unkown'
+# iterating through the rows
+for index, row in dataframe.iterrows():
+    stress = row['Stress (MPa)']
+    strain = row['Strain']
+    # ultimate = maximum of the stress
+    ultimate = max(stress)
+    # finding the index nearest to half the maximum
+    elastic_mid = np.abs(stress - round(1/2*ultimate)).argmin()
+    # selecting the 200 points to the right and left of the elastic mid
+    start, end = elastic_mid-200, elastic_mid+200
+    # doing a linear regression
+    # slope is the youngs modulus
+    slope, intercept, r_value, p_value, std_err = linregress(strain[start:end],
+                                                             stress[start:end])
+    # adding the slope and intercept back to the original dataframe
+    dataframe.at[index, 'Youngs'] = slope
+    dataframe.at[index, 'Intercepts'] = intercept
+    dataframe.at[index, 'Ultimate'] = ultimate
+    
+    # clearing variables to clean up IDE
+    del stress, strain, ultimate, elastic_mid, start, end, slope, intercept,
+    del r_value, p_value, std_err, row, index
+    
+    
+    pass
 
 
 
 
-#%% Plotting all together
+#%% plotting combined
 
+plt.figure()
 
+for index, row in dataframe.iterrows():
+    x = row['Strain']
+    y = row['Stress (MPa)']
+    sample = row['Name']
+    plt.plot(x, y, label=sample)
+    
+    pass
 
-
-
-
+plt.xlabel("Strain")
+plt.ylabel("Stress (MPa)")
+plt.title('Combined Stress Strain Curves')
+plt.legend()
+plt.xlim(0)
+plt.ylim(0)
+plt.show()
     
 
