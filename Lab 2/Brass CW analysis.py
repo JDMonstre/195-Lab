117d948645468ee653d38e0e88acefd91ec655fa
JDMonstre <joss.demonts@gmail.com>
2024-11-10 12:39:46 -0800

    Setting up data

---
+++ Lab 2/Brass CW analysis.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 12:22:48 2024
+
+@author: JDMonster
+"""
+
+import pandas as pd
+
+
+df = pd.read_excel('C:/Users/jossd/195 Lab/Lab 2/Cold Worked Brass_Tensile Data Only_MatE 195-03_2024 10 22.xlsx',
+                   sheet_name=None)
+
